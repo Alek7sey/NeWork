56ef620299f5313a291e5757219cfa0432799ec2
Alexey Ashikhmin <ash60782@gmail.com>
2023-12-17 18:22:16 +0500

    Initial commit. The layout has been made.

---
+++ app/src/main/java/ru/netology/nework/application/NeWorkApp.kt
@@ -0,0 +1,9 @@
+package ru.netology.nework.application
+
+import android.app.Application
+
+class NeWorkApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/ru/netology/nework/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.netology.nework.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
+
